9005e748b46c117dc3cb15d799d34729799684c1
Victor De Ponte <throoze@gmail.com>
2011-03-12 00:14:13 -0430

    Añadida la funcion main dummy y los includes...

---
+++ job.c
@@ -0,0 +1,20 @@
+#ifndef STD
+#define STD
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#define FALSE 0
+#define TRUE 1
+#endif
+
+#ifndef ALM
+#define ALM
+#include "almacenamiento.h"
+#endif
+
+#include "job.h"
+
+int main(int argc, char **argv){
+  printf("job: Todavía no esta implementado...\n");
+}
---
+++ job.h
@@ -0,0 +1,14 @@
+#ifndef STD
+#define STD
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#define FALSE 0
+#define TRUE 1
+#endif
+
+#ifndef ALM
+#define ALM
+#include "almacenamiento.h"
+#endif
